d625b15499fad70014a0d94fdc021fc5ea8a14b5
Philip Scott <pscott@zeptohost.com>
2017-08-24 12:59:57 -0400

    Add perlin4

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ function tumultFactory (seed) {
     new Vec3(1, 1, 0), new Vec3(-1, 1, 0), new Vec3(1, -1, 0), new Vec3(-1, -1, 0),
     new Vec3(1, 1, -1), new Vec3(-1, 1, -1), new Vec3(1, -1, -1), new Vec3(-1, -1, -1)
   ]
+  var g4 = [
+    new Vec4(0, 1, 1, 1), new Vec4(0, 1, 1, -1), new Vec4(0, 1, -1, 1), new Vec4(0, 1, -1, -1),
+    new Vec4(0, -1, 1, 1), new Vec4(0, -1, 1, -1), new Vec4(0, -1, -1, 1), new Vec4(0, -1, -1, -1),
+    new Vec4(1, 0, 1, 1), new Vec4(1, 0, 1, -1), new Vec4(1, 0, -1, 1), new Vec4(1, 0, -1, -1),
+    new Vec4(-1, 0, 1, 1), new Vec4(-1, 0, 1, -1), new Vec4(-1, 0, -1, 1), new Vec4(-1, 0, -1, -1),
+    new Vec4(1, 1, 0, 1), new Vec4(1, 1, 0, -1), new Vec4(1, -1, 0, 1), new Vec4(1, -1, 0, -1),
+    new Vec4(-1, 1, 0, 1), new Vec4(-1, 1, 0, -1), new Vec4(-1, -1, 0, 1), new Vec4(-1, -1, 0, -1),
+    new Vec4(1, 1, 1, 0), new Vec4(1, 1, -1, 0), new Vec4(1, -1, 1, 0), new Vec4(1, -1, -1, 0),
+    new Vec4(-1, 1, 1, 0), new Vec4(-1, 1, -1, 0), new Vec4(-1, -1, 1, 0), new Vec4(-1, -1, -1, 0)
+  ]
   function lerp (a, b, t) {
     return a * (1 - t) + b * t
   }
@@ -40,6 +50,10 @@ function tumultFactory (seed) {
     var hash = p[x + p[y + p[z]]] % g3.length
     return g3[hash]
   }
+  function grad4 (x, y, z, t) {
+    var hash = p[x + p[y + p[z + p[t]]]] % g4.length
+    return g4[hash]
+  }
   function Vec1 (x) {
     this.x = x
   }
@@ -52,7 +66,13 @@ function tumultFactory (seed) {
     this.y = y
     this.z = z
   }
-  Vec1.prototype.doc = function (x) {
+  function Vec4 (x, y, z, t) {
+    this.x = x
+    this.y = y
+    this.z = z
+    this.t = t
+  }
+  Vec1.prototype.dot = function (x) {
     return this.x * x
   }
   Vec2.prototype.dot = function (x, y) {
@@ -60,6 +80,9 @@ function tumultFactory (seed) {
   }
   Vec3.prototype.dot = function (x, y, z) {
     return this.x * x + this.y * y + this.z * z
+  }
+  Vec4.prototype.dot = function (x, y, z, t) {
+    return this.x * x + this.y * y + this.z * z + this.t * t
   }
 
   var i
@@ -133,6 +156,64 @@ function tumultFactory (seed) {
           fade(dy)
         ),
         fade(dz)
+      )
+    },
+    perlin4: function (x, y, z, t) {
+      var gx = Math.trunc(x) % 256
+      var gy = Math.trunc(y) % 256
+      var gz = Math.trunc(z) % 256
+      var gt = Math.trunc(t) % 256
+
+      var dx = x - gx
+      var dy = y - gy
+      var dz = z - gz
+      var dt = t - gt
+
+      var n0000 = grad4(gx, gy, gz).dot(dx, dy, dz)
+      var n1000 = grad4(gx + 1, gy, gz, gt).dot(dx - 1, dy, dz)
+      var n0100 = grad4(gx, gy + 1, gz, gt).dot(dx, dy - 1, dz)
+      var n1100 = grad4(gx + 1, gy + 1, gz, gt).dot(dx - 1, dy - 1, dz)
+      var n0010 = grad4(gx, gy, gz + 1, gt).dot(dx, dy, dz - 1)
+      var n1010 = grad4(gx + 1, gy, gz + 1, gt).dot(dx - 1, dy, dz - 1)
+      var n0110 = grad4(gx, gy + 1, gz + 1, gt).dot(dx, dy - 1, dz - 1)
+      var n1110 = grad4(gx + 1, gy + 1, gz + 1, gt).dot(dx - 1, dy - 1, dz - 1)
+      var n0001 = grad4(gx, gy, gz, gt + 1).dot(dx, dy, dz, dt - 1)
+      var n1001 = grad4(gx + 1, gy, gz, gt + 1).dot(dx - 1, dy, dz, dt - 1)
+      var n0101 = grad4(gx, gy + 1, gz, gt + 1).dot(dx, dy - 1, dz, dt - 1)
+      var n1101 = grad4(gx + 1, gy + 1, gz, gt + 1).dot(dx - 1, dy - 1, dz, dt - 1)
+      var n0011 = grad4(gx, gy, gz + 1, gt + 1).dot(dx, dy, dz - 1, dt - 1)
+      var n1011 = grad4(gx + 1, gy, gz + 1, gt + 1).dot(dx - 1, dy, dz - 1, dt - 1)
+      var n0111 = grad4(gx, gy + 1, gz + 1), gt + 1.dot(dx, dy - 1, dz - 1, dt - 1)
+      var n1111 = grad4(gx + 1, gy + 1, gz + 1, gt + 1).dot(dx - 1, dy - 1, dz - 1, dt - 1)
+
+      return lerp(
+        lerp(
+          lerp(
+            lerp(n0000, n1000, dx),
+            lerp(n0100, n1100, dx),
+            fade(dy)
+          ),
+          lerp(
+            lerp(n0010, n1010, dx),
+            lerp(n0110, n1110, dx),
+            fade(dy)
+          ),
+          fade(dz)
+        ),
+        lerp(
+          lerp(
+            lerp(n0001, n1001, dx),
+            lerp(n0101, n1101, dx),
+            fade(dy)
+          ),
+          lerp(
+            lerp(n0011, n1011, dx),
+            lerp(n0111, n1111, dx),
+            fade(dy)
+          ),
+          fade(dz)
+        ),
+        fade(dt)
       )
     }
   }
