a7a89d6588b29ea2fe6f190a61644806342bca0c
Philip Scott <pscott@zeptohost.com>
2017-09-08 21:10:54 -0400

    Upgrade terrapaint and update docs page

--- docs/demo.js
+++ docs/demo.js
@@ -1,64 +1,30 @@
 import terrapaint from 'terrapaint'
-import { Simplex2, PerlinN } from '../src/index'
+import { Simplex2 } from '../src/index'
 
 var seed = Math.random()
-var simplex = new Simplex2(seed).transform(function(x, y) {
-  return Math.abs(Math.sin((x * 3 + this.octavate(3, x, y)) * 5)) * 2 - 1
+var simplex = new Simplex2(seed)
+
+var n = simplex.gen
+var f = simplex.octavate.bind(simplex)
+var sin = Math.sin
+var cos = Math.cos
+var pow = Math.pow
+var pi = Math.PI
+var abs = Math.abs
+var e = Math.E
+
+$('#gen-noise').addEventListener('click', function () {
+  var evalStr = $('#eval-str').value
+  var transformedNoise = simplex.transform(function(x, y) {
+    return eval(evalStr)
+  })
+  var map = terrapaint.map(transformedNoise, {
+    offset: true,
+    period: 1
+  })
+  map.draw('#noise-canvas')
 })
 
-var perlin = new PerlinN(seed).transform(function(x, y) {
-  return this.octavate(2, x, y)
-})
-
-terrapaint(simplex, 256, 256, {
-  offset: true
-})
-terrapaint(perlin, 256, 256, {
-  offset: true
-})
-
-
-
-/*
- * var n = this.octavate(3, x * 2, y * 2)
- * return (Math.sin(x + n * 5) + n) / 2
- *
- * return Math.abs(this.octavate(3, x / 4, y / 4)) * 2 - 1
- *
- * return Math.sin(x * this.octavate(3, x, y) * Math.PI)
- *
- * return this.octavate(3, x + this.octavate(3, x, y), y)
- * return this.octavate(3, x + this.gen(x, y), y)
- * return this.octavate(3, x + this.gen(x, y), y * x / 10)
- * return Math.sin(this.octavate(3, Math.abs(this.gen(x, y)) * x, y))
- *
- * return Math.sin(1 / this.octavate(3, x / 4, y / 4))
- *
- * return Math.sin(x + 1 / this.octavate(3, x / 2, y / 2))
- * return Math.sin(x * 4 + this.octavate(3, x, y))
- * return Math.sin((x * 3 + this.octavate(3, x, y)) * 4)
- * return Math.abs(Math.sin((x * 3 + this.octavate(3, x, y)) * 5)) * 2 - 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import tumult from '../src/index'
-tumult.seed(Math.random())
-terrapaint(tumult.perlin2, 256, 256, {
-  offset: true
-})
-terrapaint(tumult.perlinN, 256, 256, {
-  offset: true
-})*/
+function $ (selector) {
+  return document.querySelector(selector)
+}
